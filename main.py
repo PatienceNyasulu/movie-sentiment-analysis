7ef61cd485c30f1a6fb55add221372ee559a8a19
ganyiwatakunda <118890097+ganyiwatakunda@users.noreply.github.com>
2023-10-01 16:33:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import tokenizer_from_json
+import pickle 
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+
+
 
 # Load the tokenizer
-#with open('tokenizer.json', 'r') as json_file:
-    #tokenizer = tokenizer_from_json(json_file.read())
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
 # Load the trained model
-#model = load_model('sentiment_analysis_model.h5')
-
+model = AutoModelForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
 # Function to preprocess the input text
 def preprocess_text(text):
     # Tokenize the text
@@ -29,6 +32,7 @@ def predict_sentiment(text):
 # Streamlit app
 def main():
     st.title("Movie Sentiment Analysis")
+    
     st.write("Enter a movie review to predict the sentiment (positive or negative).")
     
     # Text input box
